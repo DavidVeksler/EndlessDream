82951742106f432fd5cb3e6734a336c6bf3a2fe7
David Veksler <heroic@gmail.com>
2024-11-18 23:56:04 -0700

    try to fix layout

--- EndlessDreamBlazorWeb/wwwroot/js/site.js
+++ EndlessDreamBlazorWeb/wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿function openModal(modalId) {
+﻿// Remove the unnecessary functions
+// window.initializeChat and window.adjustChatHeight can be deleted
+
+window.scrollToBottom = (element) => {
+    if (element) {
+        element.scrollTop = element.scrollHeight;
+    }
+}
+
+function openModal(modalId) {
     var modal = new bootstrap.Modal(document.getElementById(modalId));
     modal.show();
 }
@@ -7,26 +16,5 @@ function closeModal(modalId) {
     var modal = bootstrap.Modal.getInstance(document.getElementById(modalId));
     if (modal) {
         modal.hide();
-    }
-}
-
-window.initializeChat = () => {
-    window.addEventListener('resize', adjustChatHeight);
-    adjustChatHeight();
-}
-
-window.adjustChatHeight = () => {
-    const chat = document.getElementById('chatMessages');
-    if (chat) {
-        const windowHeight = window.innerHeight;
-        const chatTop = chat.getBoundingClientRect().top;
-        const footerHeight = document.querySelector('.card-footer').offsetHeight;
-        chat.style.height = `${windowHeight - chatTop - footerHeight - 20}px`; // 20px for padding
-    }
-}
-
-window.scrollToBottom = (element) => {
-    if (element) {
-        element.scrollTop = element.scrollHeight;
     }
 }
